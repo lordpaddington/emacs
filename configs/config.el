840974c67e5e37147c465e3b38330f9066a11c42
Viktor Weyde <viktor@Viktors-FlixMac.fritz.box>
2021-03-31 15:55:22 +0200

    Bunch of Org-mode tweaks

--- configs/config.el
+++ configs/config.el
@@ -26,6 +26,8 @@
   :bind* (("C-x M-c" . restart-emacs)))
 
 ;;BASICS
+(set-face-attribute 'default nil :font "Menlo 15")
+
 
 ;;MAC SPECIFIC - put it in an ifmac
 (cond ((eq system-type 'darwin)       
@@ -244,6 +246,37 @@
   (define-key org-mode-map (kbd "<M-left>") 'beginning-of-line)
   (define-key org-mode-map (kbd "<M-right>") 'end-of-line)
   (centered-cursor-mode))
+
+
+;; Making OrgMode Nice:
+(setq org-hide-emphasis-markers t)
+(font-lock-add-keywords 'org-mode
+                            '(("^ +\\([-*]\\) "
+                               (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+;;ez valamiert nem megy az elso sorban......
+
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+
+   (let* ((variable-tuple (cond ((x-list-fonts "Menlo") '(:font "Menlo"))
+                                 ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+                                 ((x-list-fonts "Verdana")         '(:font "Verdana"))
+                                 ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+                                 (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+           (base-font-color     (face-foreground 'default nil 'default))
+           (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+
+      (custom-theme-set-faces 'user
+                              `(org-level-8 ((t (,@headline ,@variable-tuple))))
+                              `(org-level-7 ((t (,@headline ,@variable-tuple))))
+                              `(org-level-6 ((t (,@headline ,@variable-tuple))))
+                              `(org-level-5 ((t (,@headline ,@variable-tuple))))
+                              `(org-level-4 ((t (,@headline ,@variable-tuple))))
+                              `(org-level-3 ((t (,@headline ,@variable-tuple))))
+                              `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.1))))
+                              `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.25))))
+                              `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline t))))))
 
 ;; Load the locally applicable settings
 (load-file "~/.emacs.d/local.el")
