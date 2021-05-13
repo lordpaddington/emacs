df0efa90762951f14ed942d4cb5221c314b243a9
lordpaddington <weydev@gmail.com>
2021-05-13 21:08:31 +0200

    Added org-roam

--- configs/config.el
+++ configs/config.el
@@ -400,6 +400,25 @@
 (setq org-agenda-files (list todo inbox someday))
 
 
+
+
+;;Org-roam
+(use-package org-roam
+      :ensure t
+      :hook
+      (after-init . org-roam-mode)
+      :custom
+      ;; (org-roam-directory (file-truename "/path/to/org-files/"))
+      ; Take this from the local.el!!!
+      :bind (:map org-roam-mode-map
+              (("C-c n l" . org-roam)
+               ("C-c n f" . org-roam-find-file)
+               ("C-c n g" . org-roam-graph))
+              :map org-mode-map
+              (("C-c n i" . org-roam-insert))
+              (("C-c n I" . org-roam-insert-immediate))))
+
+
 ;; FUNCTIONS
 ;; =========
 
