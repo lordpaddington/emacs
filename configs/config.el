8cfd6b98fe3515a25982abcd6479ab64c82d89b2
lordpaddington <weydev@gmail.com>
2021-06-02 11:04:40 +0200

    Function to swap splitting layout.

--- configs/config.el
+++ configs/config.el
@@ -554,6 +554,20 @@
 
 ;; (setq split-window-preferred-function #'my-split-window-sensibly)
 
+(defun window-split-toggle ()
+  "Toggle between horizontal and vertical split with two windows."
+  (interactive)
+  (if (> (length (window-list)) 2)
+      (error "Can't toggle with more than 2 windows!")
+    (let ((func (if (window-full-height-p)
+                    #'split-window-vertically
+                  #'split-window-horizontally)))
+      (delete-other-windows)
+      (funcall func)
+      (save-selected-window
+        (other-window 1)
+        (switch-to-buffer (other-buffer))))))
+
 
 
 ;; LAST
